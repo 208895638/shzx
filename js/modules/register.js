9ddefd22148190b34621988961030d95512d0017
yong.zhu <208895638@qq.com>
2018-05-18 13:05:52 +0800

    增加loading

--- js/modules/register.js
+++ js/modules/register.js
@@ -3,7 +3,13 @@ layui.define(['layer', 'form', "element", "jquery"], function (exports) {
     var layer = layui.layer
     , form = layui.form,
     $ = layui.$;
-
+    // loading
+    var loading ;
+    $(document).on('ajaxStart',function(){ //使用bind
+        loading = layer.load(3, {time: 10*1000})
+     }).on('ajaxStop',function(){ //直接使用ajaxComplete
+        layer.close(loading); 
+     });
 
     var url = "../api/mapi.aspx";
     $(".loginData").on("click", function (e) {
